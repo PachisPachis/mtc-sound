e63c14b1b451c25eb382d8e5babf21bfcc5b1c04
PachisPachis <pacheco@mundo-r.com>
2018-01-27 11:46:36 +0100

    Fixed value for BackView volume attenuation
    In original 7floor code it should take the backview attenuation value from stock/factory configuration but it's not working in my PX5 unit.
    I set initial cut value to 18 because it's ok for me (and it works).

--- library/src/main/java/com/sevenfloor/mtcsound/state/BackViewState.java
+++ library/src/main/java/com/sevenfloor/mtcsound/state/BackViewState.java
@@ -2,7 +2,7 @@ package com.sevenfloor.mtcsound.state;
 
 public class BackViewState {
     public boolean active = false;
-    public int cut = 0;
+    public int cut = 18;
 
     public int getActualCut() {
         return active ? cut  : 0;
